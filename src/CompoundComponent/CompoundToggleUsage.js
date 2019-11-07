1a3c43d04b91034c6840621f1c965038906af2c8
zhunor <ozerorhun@gmail.com>
2019-11-08 02:08:26 +0300

    Fix format

--- src/CompoundComponent/CompoundToggleUsage.js
+++ src/CompoundComponent/CompoundToggleUsage.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import CompoundToggle from './CompoundToggle'
 
-const CompoundToggleUsage = ({onToggle}) => {
+const CompoundToggleUsage = ({ onToggle }) => {
   return (
     <CompoundToggle onToggle={onToggle}>
-      <CompoundToggle.On> <p>The button is on </p></CompoundToggle.On>
-      <CompoundToggle.Off><p>The button is off </p></CompoundToggle.Off>
+      <CompoundToggle.On> <p>The button is on </p> </CompoundToggle.On>
+      <CompoundToggle.Off> <p>The button is off </p> </CompoundToggle.Off>
       <CompoundToggle.Button />
     </CompoundToggle>);
 }
